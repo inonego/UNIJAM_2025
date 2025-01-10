533b3bcff052a21ed07ec7bc6f3ddc54d53caa62
stayup24h <sani0501we@gmail.com>
2025-01-10 21:45:35 +0900

    Figma Unity 연결

---
+++ Assets/Scripts/UI/start.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class star : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("dd");
+    }
+
+}
