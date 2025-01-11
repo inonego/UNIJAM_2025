cd0d38975def08b7fa59c400a68050f86f81c467
meo-young <meoyoung000@naver.com>
2025-01-11 12:31:51 +0900

    SoundManager 구현

--- Assets/Scripts/UI/TitleManager.cs
+++ Assets/Scripts/UI/TitleManager.cs
@@ -1,4 +1,4 @@
-using UnityCommunity.UnitySingleton;
+﻿using UnityCommunity.UnitySingleton;
 
 using UnityEngine;
 
@@ -6,8 +6,64 @@ using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoSingleton<TitleManager>
 {
+    [Header("# 스테이지 선택 팝업")]
+    [SerializeField] GameObject stagePopup;
+
+    [Header("# 설정 팝업")]
+    [SerializeField] GameObject settingPopup;
+
+    private void Awake()
+    {
+        if(stagePopup.activeSelf)
+            stagePopup.SetActive(false);
+
+        if(settingPopup.activeSelf)
+            settingPopup.SetActive(false);
+    }
+
+    private void Start()
+    {
+        SoundManager.instance.PlayBGM(BGM.Menu);
+    }
+
     public void LoadScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
-    } 
+        FadeManager.instance.FadeOut(onComplete: () => { SceneManager.LoadScene(sceneName); });
+    }
+
+    public void OnClickStagePopUpBtn()
+    {
+        if (!stagePopup.activeSelf)
+            stagePopup.SetActive(true);
+    }
+
+    public void DeactiveStagePopUp()
+    {
+        if (stagePopup.activeSelf)
+            stagePopup.SetActive(false);
+    }
+
+    public void OnClickSettingPopUpBtn()
+    {
+        if(!settingPopup.activeSelf)
+            settingPopup.SetActive(true);
+    }
+
+    public void DeactiveSettingPopUp()
+    {
+        if(settingPopup.activeSelf)
+            settingPopup.SetActive(false);
+    }
+
+    public void OnClickExitBtn()
+    {
+        FadeManager.instance.FadeOut(onComplete: () =>
+        {
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #else
+            Application.Quit(); // 어플리케이션 종료
+            #endif
+        });
+    }
 }
